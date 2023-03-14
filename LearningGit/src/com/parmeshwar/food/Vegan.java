7292b4e665ff32a2c1a85596dbc79c59ea2a6cb8
Parmesh <Parmesh@LAPTOP-M66PDQIA>
2023-03-14 23:06:44 +0530

    Personal Commit

---
+++ LearningGit/src/com/parmeshwar/food/Vegan.java
@@ -0,0 +1,15 @@
+package com.parmeshwar.food;
+
+public class Vegan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String milk = "Almond Milk, Soya Milk";
+		String biryani = "Jackfruit Biryani";
+			
+		System.out.println(milk);
+		System.out.println(biryani);
+
+	}
+
+}
