2a73e964371b5cd6e3742c20452567c5905863ac
Parmeshwar Kothakonda <87478621+ParmeshwarKothakonda@users.noreply.github.com>
2023-03-26 17:42:15 +0530

    Printed Haleem

--- LearningGit/src/com/parmeshwar/food/Vegan.java
+++ LearningGit/src/com/parmeshwar/food/Vegan.java
@@ -6,10 +6,11 @@ public class Vegan {
 		// TODO Auto-generated method stub
 		String milk = "Almond Milk, Soya Milk";
 		String biryani = "Jackfruit Biryani";
-		String Haleem = "Vegan Haleem";
+		String haleem = "Vegan Haleem";
 			
 		System.out.println(milk);
 		System.out.println(biryani);
+		System.out.println(haleem);
 
 	}
 
