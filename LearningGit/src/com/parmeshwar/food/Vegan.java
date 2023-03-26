194c499b6e5b7afe3420d46c8e0584f7903da0da
Parmeshwar Kothakonda <87478621+ParmeshwarKothakonda@users.noreply.github.com>
2023-03-26 17:47:45 +0530

    More Vegan dishes Added

--- LearningGit/src/com/parmeshwar/food/Vegan.java
+++ LearningGit/src/com/parmeshwar/food/Vegan.java
@@ -4,11 +4,14 @@ public class Vegan {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String milk = "Almond Milk, Soya Milk";
-		String biryani = "Jackfruit Biryani";
-			
-		System.out.println(milk);
+		
+		String rotiCurry = "Chakna Kulcha";
+		String biryani = "Handi Keema Dum Biryani (Vegan)";
+		String deserts = "3 Cats and A Monkey & Waffles with peanut butter and chocolate sauce";
+		
+		System.out.println(rotiCurry);	
 		System.out.println(biryani);
+		System.out.println(deserts);
 
 	}
 
